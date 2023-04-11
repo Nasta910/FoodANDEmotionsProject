fc89e5278361c4101ef6d72ddeb2900e84a8d4bf
Nasta910 <37406576+Nasta910@users.noreply.github.com>
2023-04-11 16:28:41 +0200

    X

--- js/script.js
+++ js/script.js
@@ -18,53 +18,57 @@ let cocktail3MoodsDuring = []
 let averageMoodCocktail1;
 let averageMoodCocktail2;
 let averageMoodCocktail3;
+let filteredData = [];
+
 async function fetchData(){
     emotionsData = await d3.csv("data/Casus_foodEmotions_data.csv");
+    for (var i = 0; i < emotionsData.length; i++) {
+        if (emotionsData[i].Disgusted != "FIT_FAILED" && emotionsData[i].Disgusted != "FIND_FAILED"){
+                filteredData.push(emotionsData[i]);
+        }
+    };
 }
 
 function putDataPerCocktail(minAge, maxAge, gender){
     cocktail1Data = [];
     cocktail2Data = [];
     cocktail3Data = [];
-    console.log(emotionsData)
-    for (var i = 0; i < emotionsData.length; i++) {
-        if (emotionsData[i].Disgusted != "FIT_FAILED" && emotionsData[i].Disgusted != "FIND_FAILED"){
-            if(gender == "empty"){
-                if(emotionsData[i].Age >= minAge && emotionsData[i].Age <= maxAge){
-                    if(emotionsData[i].Event_Marker.includes("1")){
-                        cocktail1Data.push(emotionsData[i]);
-                    }
-                    else if(emotionsData[i].Event_Marker.includes("2")){
-                        cocktail2Data.push(emotionsData[i]);
-                    }
-                    else{
-                        cocktail3Data.push(emotionsData[i]);
-                    }
+    for (var i = 0; i < filteredData.length; i++) {
+        if(gender == "empty"){
+            if(filteredData[i].Age >= minAge && filteredData[i].Age <= maxAge){
+                if(filteredData[i].Event_Marker.includes("1")){
+                    cocktail1Data.push(filteredData[i]);
                 }
-            }
-            else{
-            if(emotionsData[i].Age >= minAge && emotionsData[i].Age <= maxAge && emotionsData[i].Gender == gender){
-                if(emotionsData[i].Event_Marker.includes("1")){
-                    cocktail1Data.push(emotionsData[i]);
-                }
-                else if(emotionsData[i].Event_Marker.includes("2")){
-                    cocktail2Data.push(emotionsData[i]);
+                else if(filteredData[i].Event_Marker.includes("2")){
+                    cocktail2Data.push(filteredData[i]);
                 }
                 else{
-                    cocktail3Data.push(emotionsData[i]);
+                    cocktail3Data.push(filteredData[i]);
+                }
+            }
+        }
+        else{
+            if(filteredData[i].Age >= minAge && filteredData[i].Age <= maxAge && filteredData[i].Gender == gender){
+                if(filteredData[i].Event_Marker.includes("1")){
+                    cocktail1Data.push(filteredData[i]);
+                }
+                else if(filteredData[i].Event_Marker.includes("2")){
+                    cocktail2Data.push(filteredData[i]);
+                }
+                else{
+                    cocktail3Data.push(filteredData[i]);
                 }
             }
         }
     }
 }
-}
 
 async function createPage(){
     await fetchData();
     putDataPerCocktail(0, 200, "empty");
-    // averageCocktail1 = getAverage(cocktail1Data);
-    // averageCocktail2 = getAverage(cocktail2Data);
-    // averageCocktail3 = getAverage(cocktail3Data);
+    console.log(cocktail1Data)
+    console.log(cocktail2Data)
+    console.log(cocktail3Data)
     averageMoodCocktail1 = getAverageMood(cocktail1Data);
     averageMoodCocktail2 = getAverageMood(cocktail2Data);
     averageMoodCocktail3 = getAverageMood(cocktail3Data);
@@ -72,7 +76,7 @@ async function createPage(){
     d3.selectAll('[id="faceCocktail1"]').attr("class", averageMoodCocktail1)
     d3.selectAll('[id="faceCocktail2"]').attr("class", averageMoodCocktail2)
     d3.selectAll('[id="faceCocktail3"]').attr("class", averageMoodCocktail3)
-    getParticipants(emotionsData)
+    getParticipants(filteredData)
     
     getMoodsDuringCocktail(cocktail1Data, participantsList)
     getMoodsDuringCocktail(cocktail2Data, participantsList)
@@ -87,15 +91,16 @@ function resetData(){
     d3.selectAll('[id="faceCocktail3"]').attr("class", null)
     let minAge = document.getElementById('iMinAgeInput').value
     let maxAge = document.getElementById('iMaxAgeInput').value
-    let gender = document.getElementById('sGenderInput').value
+    let gender = String(document.getElementById('sGenderInput').value)
 
-    putDataPerCocktail(minAge, maxAge, gender);
+    putDataPerCocktail(Number(minAge), Number(maxAge), gender);
+    
     console.log(cocktail1Data)
     console.log(cocktail2Data)
     console.log(cocktail3Data)
-    console.log(cocktail1Data.length)
     if (cocktail3Data.length != 0){
         averageMoodCocktail1 = getAverageMood(cocktail1Data);
+        console.log(averageMoodCocktail1);
         averageMoodCocktail2 = getAverageMood(cocktail2Data);
         averageMoodCocktail3 = getAverageMood(cocktail3Data);
         d3.selectAll('[id="faceCocktail1"]').attr("class", averageMoodCocktail1)
@@ -121,9 +126,24 @@ function drinkAllCocktails(){
     emptyCocktail(Cocktail1Content)
     emptyCocktail(Cocktail2Content)
     emptyCocktail(Cocktail3Content)
-    showMoodChanges(cocktail1MoodsDuring, "faceCocktail1", 0, averageMoodCocktail1)
-    showMoodChanges(cocktail2MoodsDuring, "faceCocktail2", 0, averageMoodCocktail2)
-    showMoodChanges(cocktail3MoodsDuring, "faceCocktail3", 0, averageMoodCocktail3)
+    showMoodChanges(cocktail1MoodsDuring, "faceCocktail1", 0, averageMoodCocktail1, Cocktail1Content)
+    showMoodChanges(cocktail2MoodsDuring, "faceCocktail2", 0, averageMoodCocktail2, Cocktail2Content)
+    showMoodChanges(cocktail3MoodsDuring, "faceCocktail3", 0, averageMoodCocktail3, Cocktail3Content)    
+}
+
+function drinkCocktail(tableNumber){
+    if(tableNumber == 1){
+        emptyCocktail(Cocktail1Content)
+        showMoodChanges(cocktail1MoodsDuring, "faceCocktail1", 0, averageMoodCocktail1, Cocktail1Content)  
+    }
+    else if (tableNumber == 2){
+        emptyCocktail(Cocktail2Content)
+        showMoodChanges(cocktail2MoodsDuring, "faceCocktail2", 0, averageMoodCocktail2, Cocktail2Content)  
+    }
+    else{
+        emptyCocktail(Cocktail3Content)
+        showMoodChanges(cocktail3MoodsDuring, "faceCocktail3", 0, averageMoodCocktail3, Cocktail3Content)  
+    }
 }
 
 function getParticipants(data){
@@ -140,7 +160,7 @@ function getMoodsDuringCocktail(cocktail, participants){
         let highestMood;
         let participantID = participants[index];
         var moodsPerParticipant = [] 
-        let moodNumber = 1;
+        
         for (var i = 0; i < cocktail.length; i++) {
             if(cocktail[i].Participant == participantID){
             var moodsOfEntry = {Neutral: 0, Happy: 0, Sad: 0, Angry: 0, Surprised: 0, Scared: 0, Disgusted: 0}
@@ -160,9 +180,6 @@ function getMoodsDuringCocktail(cocktail, participants){
             }
             
             moodsPerParticipant.push(highestMood);
-            
-            
-            moodNumber++;
             }
             
         }
@@ -232,26 +249,8 @@ function generalizeDataDuring(data){
         }
     }
     for (let i = 0; i < numberOfMoods; i++) {  
-        var mf = 1;
-    var m = 0;
-    var item;
-        for (var ic=0; ic<fullDataExtracted[i].length; ic++)
-        {
-                for (var j=ic; j<fullDataExtracted[i].length; j++)
-                {
-                        if (fullDataExtracted[i][ic] == fullDataExtracted[i][j])
-                        m++;
-                        if (mf<m)
-                        {
-                        mf=m; 
-                        item = fullDataExtracted[i][ic];
-                        }
-                }
-                m=0;
-        }
-        moodsToReturn.push(item)
+        moodsToReturn.push(getMostFrequentValue(fullDataExtracted[i]))
     }
-    
     return moodsToReturn
     
 }
@@ -272,77 +271,30 @@ function getAverageMood(cocktailData){
         let highestMood = Object.keys(moodsOfEntry).find(key => moodsOfEntry[key] === highestMoodValue);
         Moods.push(highestMood);
     }
+    
+    return(getMostFrequentValue(Moods))
+}
+function getMostFrequentValue(data){
     var mf = 1;
     var m = 0;
     var item;
-    for (var i=0; i<Moods.length; i++)
+    for (var i=0; i<data.length; i++)
     {
-            for (var j=i; j<Moods.length; j++)
+            for (var j=i; j<data.length; j++)
             {
-                    if (Moods[i] == Moods[j])
+                    if (data[i] == data[j])
                      m++;
                     if (mf<m)
                     {
                       mf=m; 
-                      item = Moods[i];
+                      item = data[i];
                     }
             }
             m=0;
     }
-    
     return(item)
+
 }
-
-// function getAverage(cocktailData){
-//     var Moods = {Neutral: 0, Happy: 0, Sad: 0, Angry: 0, Surprised: 0, Scared: 0, Disgusted: 0}
-//     for (var i = 0; i < cocktailData.length; i++) {       
-//         var moodsOfEntry = {Neutral: 0, Happy: 0, Sad: 0, Angry: 0, Surprised: 0, Scared: 0, Disgusted: 0}
-//         moodsOfEntry.Neutral = Number(cocktailData[i].Neutral);
-//         moodsOfEntry.Happy = Number(cocktailData[i].Happy);
-//         moodsOfEntry.Sad = Number(cocktailData[i].Sad);
-//         moodsOfEntry.Angry =  Number(cocktailData[i].Angry);
-//         moodsOfEntry.Surprised = Number(cocktailData[i].Surprised);
-//         moodsOfEntry.Scared = Number(cocktailData[i].Scared);
-//         moodsOfEntry.Disgusted = Number(cocktailData[i].Disgusted);
-
-//         let highestMoodValue = Math.max(moodsOfEntry.Neutral, moodsOfEntry.Happy, moodsOfEntry.Sad, moodsOfEntry.Angry, moodsOfEntry.Surprised, moodsOfEntry.Scared, moodsOfEntry.Disgusted)
-//         let highestMood = Object.keys(moodsOfEntry).find(key => moodsOfEntry[key] === highestMoodValue);
-
-//         switch(highestMood) {
-//             case "Happy":
-//                 Moods.Happy = Moods.Happy + highestMoodValue; 
-//               break;
-//             case "Sad":
-//                 Moods.Sad = Moods.Sad + highestMoodValue;
-//               break;
-//             case "Angry":
-//                 Moods.Angry = Moods.Angry + highestMoodValue; 
-//               break;
-//             case "Surprised":
-//                 Moods.Surprised = Moods.Surprised + highestMoodValue; 
-//               break;
-//               case "Scared":
-//                 Moods.Scared = Moods.Scared + highestMoodValue; 
-//               break;
-//             case "Disgusted":
-//               Moods.Disgusted = Moods.Disgusted + highestMoodValue; 
-//               break;
-//             default:
-//                 Moods.Neutral = Moods.Neutral + highestMoodValue; 
-//           }
-//     }
-    
-//     console.log("Neutral: "+ Moods.Neutral)
-//     console.log("Happy: "+ Moods.Happy)
-//     console.log("Sad: "+ Moods.Sad)
-//     console.log("Angry: "+ Moods.Angry)
-//     console.log("Surprised: "+ Moods.Surprised)
-//     console.log("Scared: "+ Moods.Scared)
-//     console.log("Disgusted: "+ Moods.Disgusted)
-//     let highestMoodValue = Math.max(Moods.Happy, Moods.Sad, Moods.Angry, Moods.Surprised, Moods.Scared, Moods.Disgusted)
-//     let highestMood = Object.keys(Moods).find(key => Moods[key] === highestMoodValue);
-//     return(highestMood)
-// }
 
 function emptyCocktail(cocktailID){
     d3.select(cocktailID).selectAll("path")
@@ -354,10 +306,11 @@ function emptyCocktail(cocktailID){
 function fillCocktail(cocktailID){
     d3.select(cocktailID).selectAll("path")
     .transition() 
-    .style("opacity", "1");
+    .duration(4000)
+    .style("transform", "translate(0px, 0px)");
 }
 
-function showMoodChanges(moodSet, tableNumber, counter, averageMoodToReturn){
+function showMoodChanges(moodSet, tableNumber, counter, averageMoodToReturn, cocktailId){
     let moodsInCodes = []
     moodSet.forEach(element => {
         switch(element) {
@@ -393,10 +346,11 @@ function showMoodChanges(moodSet, tableNumber, counter, averageMoodToReturn){
             console.log(counter, moodSet[counter]);
         counter++;                  //  increment the counter
         if (counter < moodSet.length) {           //  if the counter < 10, call the loop function)
-            showMoodChanges(moodSet, tableNumber, counter, averageMoodToReturn);             //  ..  again which will trigger another 
+            showMoodChanges(moodSet, tableNumber, counter, averageMoodToReturn, cocktailId);             //  ..  again which will trigger another 
         }                        //  ..  setTimeout()
         else{
             d3.selectAll('[id="'+tableNumber+'"]').attr("class", averageMoodToReturn)
+            fillCocktail(cocktailId)
         }
       },2500)
 }
